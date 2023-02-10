56110807715e0a3b095bf5d7789a008e47b573d5
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2023-02-10 09:34:27 +0200

    epub-zip-mimetype-fix: refactor

--- epub-zip-mimetype-fix.c
+++ epub-zip-mimetype-fix.c
@@ -11,25 +11,19 @@ void fix_index_zero(zip_t *arc) {
   zip_set_file_compression(arc, 0, ZIP_CM_STORE, 0);
 }
 
-zip_source_t* mimetype_source(zip_t *arc) {
-  char *buf = strdup("application/epub+zip");
-  return zip_source_buffer(arc, buf, strlen(buf), 1);
-}
+void swap(zip_t *arc, int a, int b) {
+  const char *a_name = zip_get_name(arc, a, 0);
+  zip_source_t *a_src = zip_source_zip_create(arc, a, 0, 0, -1, NULL);
+  const char *b_name = zip_get_name(arc, b, 0);
+  zip_source_t *b_src = zip_source_zip_create(arc, b, 0, 0, -1, NULL);
+  warnx("swapping %d (%s) with %d (%s)", a, a_name, b, b_name);
 
-void swap_0_with_idx(zip_t *arc, int mimetype_idx) {
-  const char *displaced_name = zip_get_name(arc, 0, 0);
-  warnx("swapping index 0 (%s) with %d (mimetype)",
-        displaced_name, mimetype_idx);
+  zip_file_replace(arc, a, b_src, ZIP_FL_ENC_UTF_8);
+  zip_file_replace(arc, b, a_src, ZIP_FL_ENC_UTF_8);
+  zip_file_rename(arc, a, "566bfa62-97ff-44df-be9a-ab9d6e4b3f93", 0); // tmp
 
-  zip_source_t *displaced_src = zip_source_zip_create(arc, 0 /* index */,
-                                                        0, 0, -1, NULL);
-  zip_file_replace(arc, mimetype_idx, displaced_src, ZIP_FL_ENC_UTF_8);
-  zip_file_rename(arc, 0, "566bfa62-97ff-44df-be9a-ab9d6e4b3f93", 0);
-  zip_file_rename(arc, mimetype_idx, displaced_name, ZIP_FL_ENC_UTF_8);
-
-  zip_source_t *mimetype_src = mimetype_source(arc);
-  zip_file_replace(arc, 0, mimetype_src, 0);
-  zip_file_rename(arc, 0, "mimetype", 0);
+  zip_file_rename(arc, b, a_name, 0);
+  zip_file_rename(arc, a, b_name, 0);
 }
 
 void fix(char *file, char *error) {
@@ -41,12 +35,12 @@ void fix(char *file, char *error) {
   int idx = zip_name_locate(arc, "mimetype", 0);
 
   if (-1 == idx) {
-    snprintf(error, BUFSIZ, "no 'mimetype' file");
+    snprintf(error, BUFSIZ, "no mimetype file");
     zip_close(arc);
     return;
   }
 
-  if (0 != idx) swap_0_with_idx(arc, idx);
+  if (0 != idx) swap(arc, 0, idx);
 
   warnx("fixing index 0");
   fix_index_zero(arc);
